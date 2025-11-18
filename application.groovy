0f527d31f7d37f1e2e8e2ee6028acf0b2b63fabd
Mitul <mituldaundkar@gmail.com>
2025-11-19 00:24:24 +0530

    3-tier base

---
+++ application.groovy
@@ -0,0 +1,25 @@
+pipeline{
+    agent any
+    stages{
+        stage('PULL'){
+            steps{
+
+            }
+        }
+        stage('BUILD'){
+            steps{
+                
+            }
+        }
+        stage('PULL'){
+            steps{
+                
+            }
+        }
+        stage('PULL'){
+            steps{
+                
+            }
+        }
+    }
+}
