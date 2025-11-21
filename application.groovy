6e19de0502a174b4961775db8bb30304b77d85fc
Mitul <mituldaundkar@gmail.com>
2025-11-21 16:45:02 +0530

    3-tier

--- application.groovy
+++ application.groovy
@@ -3,22 +3,36 @@ pipeline{
     stages{
         stage('PULL'){
             steps{
-
+                git branch: 'devops', url: 'https://github.com/shubhamkalsait/EasyCRUD.git'
             }
         }
-        stage('BUILD'){
+        stage('FRONTEND DOCKER-BUILD'){
             steps{
-                
+                sh '''cd frontend
+                        docker build. -t mituldaundkar/easy-frontend:latest'''
             }
         }
-        stage('PULL'){
+        stage('BACKEND DOCKER-BUILD'){
             steps{
-                
+                sh '''cd backend
+                    docker build. -t mituldaundkar/easy-backend:latest'''
             }
         }
-        stage('PULL'){
+        stage('DOCKER-PUSH'){
             steps{
-                
+                sh '''docker push mituldaundkar/easy-frontend:latest
+                        docker push mituldaundkar/easy-backend:latest'''
+            }
+        }
+         stage('DOCKER-CLEAN'){
+            steps{
+                sh '''docker rmi mituldaundkar/easy-frontend:latest
+                        docker rmi mituldaundkar/easy-backend:latest'''
+            }
+        }
+        stage('DEPLOY'){
+            steps{
+                sh 'kubectl apply -f simple-deploy/'
             }
         }
     }
